bf92ffca4c71e55ebf7be6cc405dc60bff1a1982
omaralotabi <omv.music.a@gmail.com>
2024-06-13 21:58:20 +0300

    **First simple program With ruby**

--- first_prog.rb
+++ first_prog.rb
@@ -1,1 +1,14 @@
-print "Hello World!"
+arr = []
+puts "Enter 2 Numbers : "
+for i in 1..2 do
+    puts "Enter Number #{i} : "
+    input = gets.chomp()
+    arr << input.to_i
+end
+
+sum = 0
+for i in 0..1 do
+    sum = sum + arr[i].to_i
+end
+
+puts ("\t The summation for the numbers is: " + sum.to_s )  
